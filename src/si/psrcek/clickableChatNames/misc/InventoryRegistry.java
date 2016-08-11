28514422a9d96acac158956049a895172e8cda40
psrcek <p.srcek@gmail.com>
2016-08-12 00:17:37 +0200

    Cleaned up GUIs

--- src/si/psrcek/clickableChatNames/misc/InventoryRegistry.java
+++ src/si/psrcek/clickableChatNames/misc/InventoryRegistry.java
@@ -16,23 +16,25 @@ public class InventoryRegistry {
 	
 	public static void init() {
 		
-		website = createItemStack(Material.BOOK_AND_QUILL, "§b§lOpen website account", "§fGives you a link to the", "§fplayer's website account.");
+		website = createItemStack(Material.BOOK_AND_QUILL,     "§b§lOpen website account", "§fGives you a link to the", "§fplayer's website account.");
 		
-		plot    = createItemStack(Material.COMPASS,        "§b§lGo to plot", "§fTeleports you to", "§fthe player's plot.", "", "§6Equivalent to: §e/p h player");
+		plot    = createItemStack(Material.COMPASS,            "§b§lGo to plot", "§fTeleports you to", "§fthe player's plot.", "", "§6Equivalent to: §e/p h player");
 		
-		tpa     = createItemStack(Material.ENDER_PEARL,    "§b§lRequest to teleport", "§fSends the player a", "§fteleportation request.", "", "§6Equivalent to: §e/tpa player");
-		tpaHere = createItemStack(Material.EYE_OF_ENDER,   "§b§lRequest to teleport to you", "§fSends the player a", "§frequest to teleport to you.", "", "§6Equivalent to: §e/tpahere player");
-		tp      = createItemStack(Material.PORTAL,         "§b§lTeleport", "§fTeleports you to", "§fthe player.", "", "§6Equivalent to: §e/tp player");
-		tpo     = createItemStack(Material.ENDER_PORTAL,   "§b§lForce Teleport", "§fTeleports you to", "§fthe player, even if", "§fhe/she has teleportation disabled", "", "§6Equivalent to: §e/tpo player");
+		tpa     = createItemStack(Material.ENDER_PEARL,        "§b§lRequest to teleport", "§fSends the player a", "§fteleportation request.", "", "§6Equivalent to: §e/tpa player");
+		tpaHere = createItemStack(Material.EYE_OF_ENDER,       "§b§lRequest to teleport to you", "§fSends the player a", "§frequest to teleport to you.", "", "§6Equivalent to: §e/tpahere player");
+		tp      = createItemStack(Material.MINECART,           "§b§lTeleport", "§fTeleports you to", "§fthe player.", "", "§6Equivalent to: §e/tp player");
+		tpo     = createItemStack(Material.EXPLOSIVE_MINECART, "§b§lForce Teleport", "§fTeleports you to", "§fthe player, even if", "§fhe/she has teleportation disabled", "", "§6Equivalent to: §e/tpo player");
 		
-		kick    = createItemStack(Material.DIAMOND_BOOTS,  "§b§lKick", "§fKicks the player.", "§4Warning: you cannot", "§4specify a reason!", "", "§6Equivalent to: §e/kick player");
-		ban     = createItemStack(Material.BARRIER,        "§b§lBan", "§fBans the player.", "§4Warning: you cannot", "§4specify a reason!", "", "§6Equivalent to: §e/ban player");
+		kick    = createItemStack(Material.DIAMOND_BOOTS,      "§b§lKick", "§fKicks the player.", "§4Warning: you cannot", "§4specify a reason!", "", "§6Equivalent to: §e/kick player");
+		ban     = createItemStack(Material.BARRIER,            "§b§lBan", "§fBans the player.", "§4Warning: you cannot", "§4specify a reason!", "", "§6Equivalent to: §e/ban player");
 		
-		promote = createItemStack(Material.DIAMOND,        "§b§lPromote", "§fPromotes the player.", "", "§6Equivalent to: §e/promote player");
-		demote  = createItemStack(Material.COAL,           "§b§lDemote", "§fDemotes the player.", "", "§6Equivalent to: §e/demote player");
+		promote = createItemStack(Material.DIAMOND,            "§b§lPromote", "§fPromotes the player.", "", "§6Equivalent to: §e/promote player");
+		demote  = createItemStack(Material.COAL,               "§b§lDemote", "§fDemotes the player.", "", "§6Equivalent to: §e/demote player");
 		
-		playerInv = createInventory("§aPlayer actions", 9, website, blankSlot, plot, tpa, tpaHere);
-		staffInv = createInventory("§aPlayer actions", 18, website, blankSlot, plot, tpa, tpaHere, tp, tpo, blankSlot, kick, ban, blankSlot, promote, demote);
+		playerInv = createInventory("§aPlayer actions",  9, website, blankSlot, plot, blankSlot, tpa, tpaHere);
+		
+		staffInv  = createInventory("§aPlayer actions", 18, website, blankSlot, plot,      blankSlot, tpa,       tpaHere,   blankSlot, tp,      tpo, 
+		                                                    kick,    ban,       blankSlot, blankSlot, blankSlot, blankSlot, blankSlot, promote, demote);
 		
 	}
 	
