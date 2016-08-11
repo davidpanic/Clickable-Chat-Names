30f56539cbe9df6c51a275c2aa34bb1af36e544a
psrcek <p.srcek@gmail.com>
2016-08-11 22:29:58 +0200

    Gah, almost fixed it
    So yea umm... the previous commit was not the way to do it, i got it almost working now, but for some reason its derping up when you have donor or donor+... That should just be included in the name

--- src/si/psrcek/clickableChatNames/adapters/ClickableChatPacketAdapter.java
+++ src/si/psrcek/clickableChatNames/adapters/ClickableChatPacketAdapter.java
@@ -1,63 +1,152 @@
 package si.psrcek.clickableChatNames.adapters;
 
-import java.lang.reflect.InvocationTargetException;
-
-import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
-
+import com.redstoner.MCJSONAPI.JSONArray;
+import com.redstoner.MCJSONAPI.JSONProperty;
 import com.redstoner.MCJSONAPI.actions.ClickAction;
-import com.redstoner.MCJSONAPI.arrays.MCText;
+import com.redstoner.MCJSONAPI.exceptions.MalformedJSONException;
 import com.redstoner.MCJSONAPI.objects.ClickEvent;
+import com.redstoner.MCJSONAPI.properties.JSONObject;
+import com.redstoner.MCJSONAPI.properties.JSONString;
 
 public class ClickableChatPacketAdapter extends PacketAdapter {
 
-	public ClickableChatPacketAdapter(Plugin plugin, ListenerPriority listenerPriority, PacketType type) {
+	public ClickableChatPacketAdapter(Plugin plugin,
+			ListenerPriority listenerPriority, PacketType type) {
 		super(plugin, listenerPriority, type);
 	}
-	
+
 	@Override
 	public void onPacketSending(PacketEvent event) {
-		// get old message
-		PacketContainer packet = event.getPacket();
-		String message = packet.getStrings().read(0);
-		
-		// cancel the old packet
-		event.setCancelled(true);
-		
-		// make a new packet
-		PacketContainer chat = new PacketContainer(PacketType.Play.Server.CHAT);
-		chat.getChatComponents().write(0, WrappedChatComponent.fromJson(formatChatMessage(message)));
-		
-		// try and send the new packet
-		try {
-			ProtocolLibrary.getProtocolManager().sendServerPacket(event.getPlayer(), chat);
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
+		if (event.getPacketType() == PacketType.Play.Server.CHAT) {
+			// get the packet
+			PacketContainer packet = event.getPacket();
+			
+			//Bukkit.getLogger().info("Real message: " + packet.getChatComponents().read(0).getJson());
+			//String message = " §4§opsrekt §7→ §rderp";
+			String message = packet.getChatComponents().read(0).getJson();
+			String newMessage = null;
+			
+			try {
+				newMessage = formatChatMessage(message, event.getPlayer());
+			} catch (MalformedJSONException e) {
+				e.printStackTrace();
+			}
+			
+			Bukkit.getLogger().info("Real message: " + message);
+			Bukkit.getLogger().info("--------------------------------");
+			
+			if (newMessage != null) {
+				Bukkit.getLogger().info("Converted message: " + newMessage);
+				
+				// set the message
+				packet.getChatComponents().write(0, WrappedChatComponent.fromJson(newMessage));
+			} else {
+				Bukkit.getLogger().info("Converted message: §4null");
+			}
 		}
 	}
-	
-	private String formatChatMessage(String oldMessage) {
-		if (oldMessage.contains("→")) {
-			String name = ChatColor.stripColor(oldMessage.split("→")[0]).trim();
+
+	private String formatChatMessage(String message, Player player) throws MalformedJSONException {
+		JSONObject oldMessage = new JSONObject(message);
+		JSONArray extra = (JSONArray) oldMessage.get("extra");
+		
+		// inputs:                  000, 111111111111111111111111111111111111111111111111111, 222222222222222222222222222222222222222, 3333333333333333333333333333, 4444444444444444444444444444, 55555
+		// nick + donor+: {"extra":[" ", {"italic":true,"color":"dark_red","text":"psrekt "}, {"bold":true,"color":"gold","text":"$"}, " ",                          {"color":"gray","text":"→ "}, "ech"],  "text":""}
+		// nick + donor:  {"extra":[" ", {"italic":true,"color":"dark_red","text":"psrekt "}, {"color":"gold","text":"$"},             " ",                          {"color":"gray","text":"→ "}, "ech"],  "text":""}
+		// nick:          {"extra":[" ", {"italic":true,"color":"dark_red","text":"psrekt"},  " ",                                     {"color":"gray","text":"→ "}, "ech"],                                "text":""}
+		// donor:         {"extra":[" ", {"color":"dark_red","text":"psrcek "},               {"color":"gold","text":"$"},             " ",                          {"color":"gray","text":"→ "}, "ech"],  "text":""}
+		// donor+:        {"extra":[" ", {"color":"dark_red","text":"psrcek "},               {"bold":true,"color":"gold","text":"$"}, " ",                          {"color":"gray","text":"→ "}, "ech"],  "text":""}
+		// nothing:       {"extra":[" ", {"color":"dark_red","text":"psrcek"},                " ",                                     {"color":"gray","text":"→ "}, "ech"],                                "text":""}
+		
+		// 0    = " "
+		// 1    = name
+		// 2    = donor or space
+		// 3    = arrow or space
+		// 4    = arrow or chat
+		// 5... = empty or chat
+		
+		if (extra != null) {
+			JSONObject arrow = null;
 			
-			MCText newMessage = new MCText(oldMessage);
-			newMessage.assign(new ClickEvent(ClickAction.RUN_COMMAND, "/user " + name), 0);
+			int chatStart = 0;
+			int nameEnd = 0;
 			
-			return newMessage.toString();
-		} else {
-			return oldMessage;
+			for (int i = 1; i < extra.size(); i++) {
+				if (extra.get(i) instanceof JSONString) {
+					if (((JSONString) extra.get(i)).equals(new JSONString(" "))) {
+						arrow = (JSONObject) extra.get(i+1);
+						chatStart = i+2;
+						nameEnd = i;
+						break;
+					}
+				}
+			}
+			
+			if (chatStart != 0 && nameEnd != 0 && isArrow(arrow)) {
+				JSONArray name = new JSONArray();
+				JSONArray chat = new JSONArray();
+				
+				ClickEvent ce = new ClickEvent(ClickAction.RUN_COMMAND, "/user " + player.getName());
+				
+				for (int i = 1; i < nameEnd; i++) {
+					JSONProperty p = extra.get(i);
+					
+					if (p instanceof JSONObject) {
+						JSONObject tempObj = (JSONObject) p;
+						tempObj.set("clickEvent", ce);
+						name.add(tempObj);
+						
+					} else if (p instanceof JSONString) {
+						JSONObject tempObj = new JSONObject();
+						tempObj.set("text", p);
+						tempObj.set("clickEvent", ce);
+						name.add(tempObj);
+					}
+				}
+				
+				for (int i = chatStart; i < extra.size(); i++) {
+					JSONProperty p = extra.get(i);
+					
+					chat.add(p);
+				}
+				
+				//TODO: construct new msg
+				JSONArray newMsg = new JSONArray();
+				newMsg.addAll(name);
+				newMsg.add(new JSONString(" "));
+				newMsg.add(arrow);
+				newMsg.addAll(chat);
+				
+				return newMsg.toString();
+			}
 		}
+		return message;
 	}
 	
-//	private String stripColors(String toStrip) {
-//		return toStrip.replaceAll("§.", "");
-//	}
+	private boolean isArrow(JSONObject arrow) {
+		if (arrow == null) {
+			return false;
+		}
+		
+		if (arrow.get("color") instanceof JSONString) {
+			if (((JSONString) arrow.get("color")).equals(new JSONString("gray"))) {
+				if (arrow.get("text") instanceof JSONString) {
+					if (((JSONString) arrow.get("text")).equals(new JSONString("→ "))) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
